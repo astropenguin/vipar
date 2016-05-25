34fb8ad2cc7773634b5c842011a9d22a5bdd8564
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-05-25 15:29:54 +0900

    Add: setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# setup.sh - build vipar tasks
+# author: Akio Taniguchi (IoA, UT)
+
+# Step 0: is CASA installed?
+if ! [ `which casapy` ]; then
+    echo "ERROR: CASA is not installed or symlinks are not created"
+    echo "Please install CASA and then !create-symlinks on CASA prompt"
+    exit
+fi
+
+# Step 1: build vipar tasks
+here=`pwd`
+cd ${here}/tasks
+buildmytasks -o=../vipar.py
+
+# Step 2: loading tasks on CASA startup
+echo "execfile('${here}/vipar.py')" >> ~/.casa/init.py
