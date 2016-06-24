fa66ce1c401a527becd6b1d56110898080b0419c
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-06-24 19:14:24 +0900

    [Add] mbmathscan, mbmathmapの実装
    + スキャンとマップの演算を行うタスク

--- tasks/task_mbmathmap.py
+++ tasks/task_mbmathmap.py
@@ -16,10 +16,38 @@ else:
 # unique preamble
 
 # definition of task
-def mbmathmap(objname='aaa'):
+def mbmathmap(expression='MP0+MP1', label0='', label1=''):
     depth = [s[3] for s in inspect.stack()].index('<module>')
     mbglobals = sys._getframe(depth).f_globals
     taskname = sys._getframe().f_code.co_name
     logger.origin(taskname)
 
-    mbglobals[objname] = 'spam!'
+    mbmp_0 = mbglobals['__mbmaps__'][0 if label0=='' else label0]
+    mbmp_1 = mbglobals['__mbmaps__'][1 if label1=='' else label1]
+    mp_0 = mbmp_0['data'].data
+    mp_1 = mbmp_1['data'].data
+
+    # place holder
+    expression = expression.lower()
+    phlist_0 = ['l0', 'label0', 'm0', 'mp0', 'map0']
+    phlist_1 = ['l1', 'label1', 'm1', 'mp1', 'map1']
+    ph_0 = [ph for ph in phlist_0 if ph in expression][0]
+    expression = expression.replace(ph_0, 'mp_0')
+    try:
+        ph_1 = [ph for ph in phlist_1 if ph in expression][0]
+        expression = expression.replace(ph_1, 'mp_1')
+    except:
+        logger.post('second map is not spacified', 'WARN')
+
+    # calculation
+    try:
+        mp_new = eval(expression)
+    except:
+        logger.post('an error occured', 'ERROR')
+
+    mbmp_0['data'].data = mp_new
+    mbmp_0.recordtask(taskname)
+    mbglobals['__mbmaps__'].append(mbmp_0)
+
+    logger.post('calculation is successfully finished')
+    logger.post('new MBMap is now stored as __mbmaps__[0]')
--- tasks/task_mbmathscan.py
+++ tasks/task_mbmathscan.py
@@ -16,10 +16,38 @@ else:
 # unique preamble
 
 # definition of task
-def mbmathscan(objname='aaa'):
+def mbmathscan(expression='SC0+SC1', label0='', label1=''):
     depth = [s[3] for s in inspect.stack()].index('<module>')
     mbglobals = sys._getframe(depth).f_globals
     taskname = sys._getframe().f_code.co_name
     logger.origin(taskname)
 
-    mbglobals[objname] = 'spam!'
+    mbsc_0 = mbglobals['__mbscans__'][0 if label0=='' else label0]
+    mbsc_1 = mbglobals['__mbscans__'][1 if label1=='' else label1]
+    sc_0 = mbsc_0['data'].data
+    sc_1 = mbsc_1['data'].data
+
+    # place holder
+    expression = expression.lower()
+    phlist_0 = ['l0', 'label0', 's0', 'sc0', 'scan0']
+    phlist_1 = ['l1', 'label1', 's1', 'sc1', 'scan1']
+    ph_0 = [ph for ph in phlist_0 if ph in expression][0]
+    expression = expression.replace(ph_0, 'sc_0')
+    try:
+        ph_1 = [ph for ph in phlist_1 if ph in expression][0]
+        expression = expression.replace(ph_1, 'sc_1')
+    except:
+        logger.post('second scan is not spacified', 'WARN')
+
+    # calculation
+    try:
+        sc_new = eval(expression)
+    except:
+        logger.post('an error occured', 'ERROR')
+
+    mbsc_0['data'].data = sc_new
+    mbsc_0.recordtask(taskname)
+    mbglobals['__mbscans__'].append(mbsc_0)
+
+    logger.post('calculation is successfully finished')
+    logger.post('new MBScan is now stored as __mbscans__[0]')
