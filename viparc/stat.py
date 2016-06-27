8f2bd76ed34e73f712830873aac399e5c1ec6b45
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-06-27 15:48:42 +0900

    [Update] stat.pyのGaussian2Dに例外処理を追加

--- viparc/stat.py
+++ viparc/stat.py
@@ -53,12 +53,21 @@ class CheckConvergence(object):
         d_diff = np.linalg.norm(p_now - self.p_prev)
         ratio = d_diff / d_now
         self.p_prev = p_now
-        print(ratio)
 
         return ratio <= self.threshold
 
 
 class Gaussian2D(object):
+    def __init__(self, b_0=23.0, threshold=3.0):
+        '''Gaussian 2D fit class.
+
+        Args:
+        - b_0 (float): typical beam size for initial guess [arcsec]
+        - threshold (float): threshold of S/N
+        '''
+        self.b_0 = float(b_0)
+        self.threshold = threshold
+
     def fit(self, mg_daz, mg_del, mp_data, b_0=23.0, threshold=3.0):
         '''Fit 2D Gaussian to a map.
 
@@ -66,25 +75,22 @@ class Gaussian2D(object):
         - mg_daz (2D array): meshgrid of dAz [arcsec]
         - mg_del (2D array): meshgrid of dEl [arcsec]
         - mp_data (2D array): data map [arbitrary unit]
-        - b_0 (float): typical beam size for initial guess [arcsec]
-        - threshold (float): threshold of S/N
 
         Returns:
-        - mp_fit (2D array): fit map [arbitrary unit]
         - hd_fit (Header): FITS header object containing the results of fit
+        - mp_fit (2D array): fit map [arbitrary unit]
         '''
         # preprocessing
         mp_data = deepcopy(mp_data)
         mp_data[np.isnan(mp_data)] = np.nanmedian(mp_data)
+        self.mg_daz, self.mg_del = mg_daz, mg_del
+        self.mp_data = mp_data
 
         # step 1: initial parameters (*_0)
-        b_0 = float(b_0)
-        bmaj_0, bmin_0, pa_0 = b_0, b_0, 0.0
+        bmaj_0, bmin_0, pa_0 = self.b_0, self.b_0, 0.0
 
-        grid_daz = np.mean(np.diff(mg_daz))
-        grid_del = np.mean(np.diff(mg_del))
-        grid_typ = np.sqrt(grid_daz**2 + grid_del**2)
-        sigma = b_0/(2*np.sqrt(np.log(2)))/grid_typ
+        gridsize = [np.mean(np.diff(mg)) for mg in (mg_daz, mg_del)]
+        sigma = self.b_0 / (2*np.sqrt(np.log(2))) / np.linalg.norm(gridsize)
 
         mp_gauss = gaussian_filter(mp_data, sigma)
         peak_pos = np.max(mp_gauss) - np.median(mp_gauss)
@@ -93,57 +99,54 @@ class Gaussian2D(object):
         j, i = np.unravel_index(argfunc(mp_gauss), mp_gauss.shape)
         xp_0, yp_0 = mg_daz[j,i], mg_del[j,i]
 
-        f = self.partialfunc(xp=xp_0, yp=yp_0, bmaj=bmaj_0, bmin=bmin_0, pa=pa_0)
+        f = self.partial(xp=xp_0, yp=yp_0, bmaj=bmaj_0, bmin=bmin_0, pa=pa_0)
         popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten())
         ampl_0, offset_0 = popt
 
         # step 2: if initial S/N<3, then stop fitting
-        sd_0 = self._estimate_sd(mg_daz, mg_del, mp_data, xp_0, yp_0, bmaj_0, bmin_0)
+        sd_0 = self._estimate_sd(xp_0, yp_0, bmaj_0, bmin_0)
         sn_0 = np.abs(ampl_0/sd_0)
         if sn_0 < threshold:
-            mp_fit = f((mg_daz, mg_del), *popt).reshape(mp_data.shape)
-            hd_fit = fits.Header()
-            hd_fit['FIT_FUNC'] = 'Gaussian2D', 'fitting function'
-            hd_fit['FIT_STAT'] = 'failed', 'fiting status'
-
-            return hd_fit, mp_fit
+            return self._failed_results()
 
         # step 3: iterative fit
-        cc = CheckConvergence()
-        while not cc(xp_0, yp_0, bmaj_0, bmin_0, pa_0, ampl_0, offset_0):
-            # bmaj, bmin, pa
-            f = self.partialfunc(xp=xp_0, yp=yp_0, ampl=ampl_0, offset=offset_0)
-            pinit = [bmaj_0, bmin_0, pa_0]
-            popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten(), pinit)
-            bmaj_0, bmin_0, pa_0 = popt
-            bmaj_e, bmin_e, pa_e = np.sqrt(np.diag(pcov))
-            if bmaj_0 < bmin_0:
-                bmaj_0, bmin_0 = bmin_0, bmaj_0
-                bmaj_e, bmin_e = bmin_e, bmaj_e
-                pa_0 += 90.0
+        try:
+            cc = CheckConvergence()
+            while not cc(xp_0, yp_0, bmaj_0, bmin_0, pa_0, ampl_0, offset_0):
+                # bmaj, bmin, pa
+                f = self.partial(xp=xp_0, yp=yp_0, ampl=ampl_0, offset=offset_0)
+                pinit = [bmaj_0, bmin_0, pa_0]
+                popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten(), pinit)
+                bmaj_0, bmin_0, pa_0 = popt
+                bmaj_e, bmin_e, pa_e = np.sqrt(np.diag(pcov))
+                if bmaj_0 < bmin_0:
+                    bmaj_0, bmin_0 = bmin_0, bmaj_0
+                    bmaj_e, bmin_e = bmin_e, bmaj_e
+                    pa_0 += 90.0
 
-            pa_0 %= 180.0
+                pa_0 %= 180.0
 
-            # xp, yp
-            f = self.partialfunc(bmaj=bmaj_0, bmin=bmin_0, pa=pa_0, ampl=ampl_0, offset=offset_0)
-            pinit = [xp_0, yp_0]
-            popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten(), pinit)
-            xp_0, yp_0 = popt
-            xp_e, yp_e = np.sqrt(np.diag(pcov))
+                # xp, yp
+                f = self.partial(bmaj=bmaj_0, bmin=bmin_0, pa=pa_0, ampl=ampl_0, offset=offset_0)
+                pinit = [xp_0, yp_0]
+                popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten(), pinit)
+                xp_0, yp_0 = popt
+                xp_e, yp_e = np.sqrt(np.diag(pcov))
 
-            # ampl, offset
-            f = self.partialfunc(xp=xp_0, yp=yp_0, bmaj=bmaj_0, bmin=bmin_0, pa=pa_0)
-            pinit = [ampl_0, offset_0]
-            popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten(), pinit)
-            ampl_0, offset_0 = popt
-            ampl_e, offset_e = np.sqrt(np.diag(pcov))
+                # ampl, offset
+                f = self.partial(xp=xp_0, yp=yp_0, bmaj=bmaj_0, bmin=bmin_0, pa=pa_0)
+                pinit = [ampl_0, offset_0]
+                popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten(), pinit)
+                ampl_0, offset_0 = popt
+                ampl_e, offset_e = np.sqrt(np.diag(pcov))
+        except:
+            return self._failed_results()
 
         # step 4: fit all parameters
-        f = self.partialfunc()
+        f = self.partial()
         pinit = np.array([xp_0, yp_0, bmaj_0, bmin_0, pa_0, ampl_0, offset_0])
         psigma = np.array([xp_e, yp_e, bmaj_e, bmin_e, pa_e, ampl_e, offset_e])
         pbounds = (pinit-psigma, pinit+psigma)
-
         popt, pcov = curve_fit(f, (mg_daz, mg_del), mp_data.flatten(), pinit, bounds=pbounds)
         xp, yp, bmaj, bmin, pa, ampl, offset = popt
         xp_e, yp_e, bmaj_e, bmin_e, pa_e, ampl_e, offset_e = np.sqrt(np.diag(pcov))
@@ -155,15 +158,14 @@ class Gaussian2D(object):
         pa %= 180.0
 
         mp_fit = f((mg_daz, mg_del), *popt).reshape(mp_data.shape)
-        sd = self._estimate_sd(mg_daz, mg_del, mp_data, xp, yp, bmaj, bmin)
+        sd = self._estimate_sd(xp, yp, bmaj, bmin)
         sn = np.abs(ampl/sd)
         chi2 = np.sum(((mp_data-mp_fit)/sd)**2) / mp_data.size
 
         # step 5: make header
-        hd_fit = fits.Header()
-        hd_fit['FIT_FUNC'] = 'Gaussian2D', 'fitting function'
-
         if sn > threshold:
+            hd_fit = fits.Header()
+            hd_fit['FIT_FUNC'] = 'Gaussian2D', 'fitting function'
             hd_fit['FIT_STAT'] = 'success', 'fiting status'
             hd_fit['FIT_CHI2'] = chi2, 'reduced Chi^2 (no unit)'
             hd_fit['FIT_SD']   = sd, 'S.D. of map (no unit)'
@@ -182,10 +184,10 @@ class Gaussian2D(object):
             hd_fit['ERR_PA']   = pa_e, 'position angle (degree)'
             hd_fit['ERR_AMPL'] = ampl_e, 'amplitude (no unit)'
             hd_fit['ERR_OFFS'] = offset_e, 'offset (no unit)'
-        else:
-            hd_fit['FIT_STAT'] = 'failed', 'fiting status'
 
-        return hd_fit, mp_fit
+            return hd_fit, mp_fit
+        else:
+            return self._failed_results()
 
     @staticmethod
     def func((x, y), xp, yp, bmaj, bmin, pa, ampl, offset):
@@ -219,7 +221,7 @@ class Gaussian2D(object):
 
         return g.flatten()
 
-    def partialfunc(self, **kwargs):
+    def partial(self, **kwargs):
         '''Return a new function with some args fixed.
 
         Args:
@@ -250,14 +252,10 @@ class Gaussian2D(object):
 
         return func
 
-    @staticmethod
-    def _estimate_sd(mg_daz, mg_del, mp_data, xp, yp, bmaj, bmin):
+    def _estimate_sd(self, xp, yp, bmaj, bmin):
         '''Estimate S.D. of data using current fit parameters.
 
         Args:
-        - mg_daz (2D array): meshgrid of dAz [arcsec]
-        - mg_del (2D array): meshgrid of dEl [arcsec]
-        - mp_data (2D array): data map [arbitrary unit]
         - xp (float): Peak x (dAz) position of 2D Gaussian [arbitrary unit]
         - yp (float): Peak y (dEl) position of 2D Gaussian [arbitrary unit]
         - bmaj (float): Major beam size of 2D Gaussian [arbitrary unit]
@@ -266,9 +264,27 @@ class Gaussian2D(object):
         Returns:
         - sd_est (float): Estimated S.D. of data [arbitrary unit]
         '''
-        w = bmaj + bmin
-        mp_mask = (mg_daz>xp-w) & (mg_daz<xp+w) & (mg_del>yp-w) & (mg_del<yp+w)
-        mp_noise = np.ma.array(mp_data, mask=mp_mask)
+        width = bmaj + bmin
+        mp_mask_daz = (self.mg_daz>xp-width) & (self.mg_daz<xp+width)
+        mp_mask_del = (self.mg_del>yp-width) & (self.mg_del<yp+width)
+        mp_noise = np.ma.array(self.mp_data, mask=(mp_mask_daz & mp_mask_del))
         sd_est = np.std(mp_noise)
 
         return sd_est
+
+    def _failed_results(self):
+        '''Return results in case of failed fit.
+
+        Args:
+        this method requires no arguments.
+
+        Returns:
+        - hd_fit (Header): FITS header object containing the results of fit
+        - mp_fit (2D array): map with nan values [arbitrary unit]
+        '''
+        mp_fit = np.full(self.mp_data.shape, np.nan)
+        hd_fit = fits.Header()
+        hd_fit['FIT_FUNC'] = 'Gaussian2D', 'fitting function'
+        hd_fit['FIT_STAT'] = 'failed', 'fiting status'
+
+        return hd_fit, mp_fit
